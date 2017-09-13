49335a9877854101c5ac4dd827899115d9ff4fcb
Zerahi <nathan_nsk@hotmail.com>
2017-09-13 09:57:03 -0500

    Fix

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class WordProcessor
-{
-	public void Main()
-	{
-	}
-}
--- Text Editor/Text Editor/Properties/Resources.Designer.cs
+++ Text Editor/Text Editor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfApp1.Properties
+namespace Text_Editor.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WpfApp1.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApp1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Text_Editor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
