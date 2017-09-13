fee2f7ebee686ec7cb8f5f3bd9221c1558af612d
Zerahi <nathan_nsk@hotmail.com>
2017-09-13 15:47:03 -0500

    Update

--- Text Editor/Text Editor/Back.cs
+++ Text Editor/Text Editor/Back.cs
@@ -12,56 +12,118 @@ namespace Text_Editor
 {
     public partial class Back : Form
     {
+        private int TabCount = 0;
+
         public Back()
         {
             InitializeComponent();
         }
 
-        #region Methods
+    #region Methods
+        #region Tabs
+        private void AddTab()
+        {
+            RichTextBox Body = new RichTextBox();
+            Body.Name = "Body";
+            Body.Dock = DockStyle.Fill;
+            Body.ContextMenuStrip = contextMenuStrip1;
 
-            #region Tabs
+            TabPage NewPage = new TabPage();
+            TabCount += 1;
 
-            private void AddTab()
+            string DocumentText = "Document " + TabCount;
+            NewPage.Name = DocumentText;
+            NewPage.Text = DocumentText;
+            NewPage.Controls.Add(Body);
+
+            tabControl1.TabPages.Add(NewPage);
+        }
+
+        private void RemoveTab()
+        {
+            if (tabControl1.TabPages.Count != 1)
             {
-
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
             }
-
-            private void RemoveTab()
+            else
             {
-
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+                AddTab();
             }
+        }
 
-            private void AddAllTabs()
+        private void RemoveAllTabs()
+        {
+            foreach (TabPage Page in tabControl1.TabPages)
             {
-
+                tabControl1.TabPages.Remove(Page);
             }
+            AddTab();
+        }
 
+        private void RemoveAllTabsButThisOne()
+        {
+            foreach (TabPage Page in tabControl1.TabPages)
+            {
+                if (Page.Name != tabControl1.SelectedTab.Name)
+                {
+                    tabControl1.TabPages.Remove(Page);
+                }
+            }
+        }
         #endregion
 
         #region SaveAndOpen
-
         private void Save()
         {
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "RTF|*.rtf";
+            saveFileDialog1.Title = "Save";
 
+            if ( saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName.Length > 0)
+                {
+                    GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                }
+            }
         }
         private void SaveAs()
         {
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            saveFileDialog1.Filter = "Text Files|*.txt|VB Files|*.vb|C# Files|*.cs|All Files|*.*";
+            saveFileDialog1.Title = "Save As";
 
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName.Length > 0)
+                {
+                    GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                }
+            }
         }
         private void Open()
         {
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.Filter = "RTF|*.rtf|Text Files|*.txt|VB Files|*.vb|C# Files|*.cs|All Files|*.*";
 
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                GetCurrentDocument.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+            }
         }
-
-
-            #endregion
         #endregion
+        #region Properties
 
-
-        private void AddAllTabsButThis()
+        private RichTextBox GetCurrentDocument
         {
-
+            get { return (RichTextBox)tabControl1.SelectedTab.Controls["Body"]; }
         }
+
+        #endregion
+    #endregion
 
         private void Form1_Load(object sender, EventArgs e)
         {
